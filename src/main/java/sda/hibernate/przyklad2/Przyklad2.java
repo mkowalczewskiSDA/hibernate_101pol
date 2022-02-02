50d9a373f91c4c2e7110851c3d1f72b71d7bf452
Michal Kowalczewski <m.kowalczewski2@gmail.com>
2022-02-02 19:46:37 +0100

    merge

--- src/main/java/sda/hibernate/przyklad2/Przyklad2.java
+++ src/main/java/sda/hibernate/przyklad2/Przyklad2.java
@@ -7,7 +7,8 @@ import sda.hibernate.util.HibernateUtil;
 
 public class Przyklad2 {
     public static void main(String[] args) {
-        usePersist();
+        //usePersist();
+        useMerge();
     }
 
 //persist() zawsze tworzy nowy obiekt
@@ -23,8 +24,24 @@ public class Przyklad2 {
         session.flush();
         session.close();
     }
-
+//merge() aktualizuje lub dodaje nowy obiekt obiektu Session
+// Służy do synchronizowania stanu obiektu w kontekście utrwalania. W przeciwieństwie do poprzedniej metody encja przekazana w argumencie nie jest dołączana do
+// zbioru obiektów zarządzanych. Jest ona kopiowana, kopia trafia pod zarząd EntityManagera lub Session i jest zwracana.
+// W momencie zakończenia transakcji wywoływany jest INSERT albo UPDATE w zależności czy istnieje rekord o danym identyfikatorze w bazie danych.*/
+// W tym miejscu warto też wspomnieć o update() - update zadziała tylko wtedy gdy do naszej sesji nie spersistowaliśmy danego obiektu, kiedy obiekt będzie załadowany dostaniemy exception
+// W przypadku merge jest to bez znaczenia jednakże jest on ciut mniej wydajny - w tym miejscu po prostu trzeba odpowiedzieć sobie na pytanie, czy dany obiekt będzie znajdował się w sesji
+// gdy będę chciał go modyfikować (raczej dość rzadka sytuacja)
     public static void useMerge() {
-
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        session.beginTransaction();
+        Country country = new Country();
+        country.setId(1);
+        country.setAlias("ES");
+        session.merge(country);
+        country.setName("Spain Changed");
+        session.flush();
+        session.close();
     }
+
+    public void useUpdate()
 }
