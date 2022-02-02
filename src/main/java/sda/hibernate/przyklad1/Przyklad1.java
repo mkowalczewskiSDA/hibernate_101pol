9d617c5b2d847dfc681764ce441964f055d9f636
Michal Kowalczewski <m.kowalczewski2@gmail.com>
2022-02-02 18:33:23 +0100

    przyklad1

--- src/main/java/sda/hibernate/przyklad1/Przyklad1.java
+++ src/main/java/sda/hibernate/przyklad1/Przyklad1.java
@@ -3,11 +3,15 @@ package sda.hibernate.przyklad1;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import sda.hibernate.model.Country;
 
 public class Przyklad1 {
     public static void main(String[] args) {
         SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
         Session session = sessionFactory.openSession();
+        Country country = session.find(Country.class, 1);
+        System.out.println(country.getName());
         session.close();
+        sessionFactory.close();
     }
 }
