e648a81827c3285bba7834835afeb862435a19cc
Michal Kowalczewski <m.kowalczewski2@gmail.com>
2022-02-02 18:42:44 +0100

    przyklad1

--- src/main/java/sda/hibernate/model/Country.java
+++ src/main/java/sda/hibernate/model/Country.java
@@ -12,8 +12,11 @@ public class Country {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "CO_ID")
     private int id;
+    @Column(name = "CO_NAME")
     private String name;
+    @Column(name = "CO_ALIAS")
     private String alias;
 
 }
