6f21667f008c27b9e7adb91bd0af8ec9970a8fbb
Michal Kowalczewski <m.kowalczewski2@gmail.com>
2022-02-03 18:01:07 +0100

    renaming columns

--- src/main/java/sda/hibernate/model/Address.java
+++ src/main/java/sda/hibernate/model/Address.java
@@ -12,17 +12,17 @@ public class Address {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "CO_ID")
+    @Column(name = "ADD_ID")
     private int id;
-    @Column(name = "CO_STREET")
+    @Column(name = "ADD_STREET")
     private String street;
-    @Column(name = "CO_BUILDING_NO")
+    @Column(name = "ADD_BUILDING_NO")
     private String buildingNo;
-    @Column(name = "CO_APARTMENT_NO")
+    @Column(name = "ADD_APARTMENT_NO")
     private String apartmentNo;
-    @Column(name = "CO_CITY")
+    @Column(name = "ADD_CITY")
     private String city;
-    @Column(name = "CO_POSTAL_CODE")
+    @Column(name = "ADD_POSTAL_CODE")
     private String postalCode;
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "ADD_CO_ID", referencedColumnName = "CO_ID")
